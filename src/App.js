31ee2c9afa0c516a38d03984dae0e294b88a338a
Jenny Zhong <zhongjenn@gmail.com>
2021-09-01 05:22:11 -0400

    adding files

---
+++ src/App.js
@@ -0,0 +1,55 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
+require('dotenv').config();
+
+
+export default function App() {
+	
+	
+	const [lat, setLat] = useState([]);
+	const [long, setLong] = useState([]);
+	const [data, setData] = useState([]);
+	
+	useEffect(() => {
+		const fetchData = async () => {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				setLat(position.coords.latitude);
+				setLong(position.coords.longitude);
+			});
+		
+			console.log("Latitude is:", lat)
+			console.log("Longitude is:", long)
+		
+ 		await fetch(`${process.env.REACT_APP_API_URL}?location=${lat},${long}&timesteps=1d&units=imperial&apikey=${process.env.REACT_APP_API_KEY}&fields=treeIndex,weedIndex,grassIndex`)
+		 .then(res => res.json())
+		 .then(result => {
+			setData(result)
+			console.log(result);
+		}); 
+		}
+	fetchData();
+	
+	}, [lat, long]);
+	
+	return (
+		
+		<div className="App">
+			{(typeof data.data != 'undefined') ? (
+				<Weather weatherData={data}/>
+			): (
+				<div>
+					<Dimmer active>
+						<Loader>Loading..</Loader>
+					</Dimmer>
+				
+				</div>
+			)}
+     
+		</div>
+	);
+}
+
+
---
+++ src/components/weather.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './styles.css';
+//import 'semantic-ui-css/semantic.min.css';
+//import { Card } from 'semantic-ui-react';
+import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+const iconPath = process.env.PUBLIC_URL + '/assets/color/';
+const pollenCount = ["None", "Very Low", "Low", "Medium", "High", "Very High"];
+
+
+const refresh = () => {
+  window.location.reload();
+}
+
+const WeatherCard = ({weatherData}) => (
+   <div className="main">
+	   <div className="top">	
+        <p className="header"> Pollen Forecast </p>
+		<Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+      </div> 
+	  
+	  <div class="break"></div>
+	  
+		<div className="item">
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[0].startTime).format("ddd")}</p>	
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[0].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[0].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[0].values.grassIndex]}</p>
+		</div>
+		
+		<div className="item-wide">
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[1].startTime).format("ddd")}</p>
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[1].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[1].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[1].values.grassIndex]}</p>
+		</div>
+		<div className="item">
+		
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[2].startTime).format("ddd")}</p>
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[2].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[2].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[2].values.grassIndex]}</p>
+		</div>
+		
+		<div className="item-wide">
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[3].startTime).format("ddd")}</p>
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[3].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[3].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[3].values.grassIndex]}</p>
+		</div>
+		
+		<div className="item">
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[4].startTime).format("ddd")}</p>
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[4].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[4].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[4].values.grassIndex]}</p>
+		</div>
+        
+		<div className="item-wide">
+		<p className="temp">{moment(weatherData.data.timelines[0].intervals[5].startTime).format("ddd")}</p>
+		<p className="temp">Weed: {pollenCount[weatherData.data.timelines[0].intervals[5].values.weedIndex]}</p>
+		<p className="temp">Tree: {pollenCount[weatherData.data.timelines[0].intervals[5].values.treeIndex]}</p>
+		<p className="temp">Grass: {pollenCount[weatherData.data.timelines[0].intervals[5].values.grassIndex]}</p>
+		</div>
+		
+  </div>
+)
+/* <p className="temp">Temperature: &deg;F</p>
+		<p className="temp">Humidity:  %</p> */
+
+export default WeatherCard;
